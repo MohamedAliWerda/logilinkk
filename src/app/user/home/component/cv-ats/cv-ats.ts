5b470236c0e589b4cda2aae67f1a647058a86b40
unknown <medaliwerda04@gmail.com>
2026-03-12 13:18:58 +0100

    master

---
+++ src/app/user/home/component/cv-ats/cv-ats.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cv-ats',
+  imports: [],
+  templateUrl: './cv-ats.html',
+  styleUrl: './cv-ats.css',
+})
+export class CvAts {
+
+}
---
+++ src/app/user/home/component/matching/matching.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-matching',
+  imports: [],
+  templateUrl: './matching.html',
+  styleUrl: './matching.css',
+})
+export class Matching {
+
+}
